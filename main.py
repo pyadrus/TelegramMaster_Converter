b5241ffa6b00ed3a28cd47e82a920dfdff255de9
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-02-29 00:51:36 +0300

    Добавлено сканирование папки с session файлами в асинхронном режиме. Как только файл session копируется в папку с session файлами, то выводится сообщение в терминал о новой session.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import os
+from datetime import datetime
 
+import aiofiles
 from loguru import logger  # https://github.com/Delgan/loguru
 from opentele.api import UseCurrentSession  # https://opentele.readthedocs.io/en/latest/#installation
 from opentele.tl import TelegramClient
@@ -22,12 +24,29 @@ def scan_session_files(session_path):
     return entities
 
 
+async def wait_and_read():
+    seen_files = set()
+
+    while True:
+        for entry in os.scandir('session'):
+            if entry.is_file() and entry.name.endswith(".session"):
+                if entry.path not in seen_files:
+                    seen_files.add(entry.path)
+                    # File has appeared, get the current time
+                    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    # Output information about the new session
+                    print(f'[{current_time}] New session file: {entry.name}')
+
+        await asyncio.sleep(1)
+
+
 async def main():
-    """Main function"""
+    """Основное меню программы"""
     print('[medium_violet_red]TelegramMaster_Converter 28.02.2024 v.0.0.1\n\n'
           '[red][1] - Запустить конвертацию\n'
           '[red][2] - Путь к папке с сессиями\n'
-          '[red][3] - Путь к папке с tdata\n')
+          '[red][3] - Путь к папке с tdata\n'
+          '[red][4] - Запуск конвертера в асинхронном режиме\n\n')
 
     user_input = input('Выберите пункт меню: ')
     if user_input == '1':
@@ -47,18 +66,25 @@ async def main():
             tdesk.SaveTData(f"{tdata_path}/{e}/tdata")
 
     elif user_input == '2':
-        print('Настройки')
+        print('[chartreuse2]Запись пути к папке с сессиями')
 
         user_input = input("Введите путь к папке с сессиями: ")
         session_path = rf"{user_input}"
         write_config(session_path)
 
     elif user_input == '3':
-        print('Настройки')
+        print('[chartreuse2]Запись пути к папке с tdata')
 
         user_input = input("Введите путь к папке с tdata: ")
         tdata_path = rf"{user_input}"
         write_config2(tdata_path)
+
+    elif user_input == '4':
+
+        print('[chartreuse2] Запуск конвертации в асинхронном режиме')
+        # Запускаем задачу на чтение файла
+        # await asyncio.create_task(wait_and_read('session'))
+        await wait_and_read()
 
 
 asyncio.run(main())
