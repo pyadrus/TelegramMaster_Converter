ddee9e73bf85203b4f9e390b63df6b2761b90cef
vitaliyzh92 <zh.vitaliy92@yandex.ru>
2024-03-01 21:04:49 +0300

    Оптимизация кода

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import asyncio
 import os
 from datetime import datetime
 
-import aiofiles
 from loguru import logger  # https://github.com/Delgan/loguru
 from opentele.api import UseCurrentSession  # https://opentele.readthedocs.io/en/latest/#installation
 from opentele.tl import TelegramClient
@@ -13,8 +12,10 @@ from config.config_manager import read_config, write_config, write_config2, read
 logger.add("log/log.log")
 
 
-def scan_session_files(session_path):
-    """Returns a list of account names in the specified session path."""
+def scan_session_files(session_path) -> list:
+    """
+    Возвращает список имен учетных записей по указанному пути сеанса.
+    """
     entities = []
     for entry in os.scandir(session_path):
         if entry.is_file() and entry.name.endswith(".session"):
@@ -22,6 +23,25 @@ def scan_session_files(session_path):
             print(f"Found accounts: {file}.session")
             entities.append(file)
     return entities
+
+
+async def convert_session_to_tdata() -> None:
+    """
+    Преобразует файлы сеанса по указанному пути сеанса в файлы tdata.
+    :return: None
+    """
+    session_path = read_config()
+    logger.info(f"{session_path}")
+    entities = scan_session_files(session_path)
+    for e in entities:
+        logger.debug(e)
+        client = TelegramClient(f"{session_path}/{e}")
+        logger.info(f"{client}")
+        tdesk = await client.ToTDesktop(flag=UseCurrentSession)
+        logger.info(f"{tdesk}")
+        logger.info(f"Saving Tdata/tdata")
+        tdata_path = read_config_tdata()
+        tdesk.SaveTData(f"{tdata_path}/{e}/tdata")
 
 
 async def wait_and_read():
@@ -35,7 +55,7 @@ async def wait_and_read():
                     # File has appeared, get the current time
                     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                     # Output information about the new session
-                    print(f'[{current_time}] New session file: {entry.name}')
+                    print(f'[{current_time}] : {entry.name}')
 
         await asyncio.sleep(1)
 
@@ -49,42 +69,25 @@ async def main():
           '[red][4] - Запуск конвертера в асинхронном режиме\n\n')
 
     user_input = input('Выберите пункт меню: ')
-    if user_input == '1':
-        print('Запуск конвертации')
+    if user_input == '1':  # Запустить конвертацию
+        print('[chartreuse2]Запуск конвертации')
+        await convert_session_to_tdata()
 
-        session_path = read_config()
-        logger.info(f"{session_path}")
-        entities = scan_session_files(session_path)
-        for e in entities:
-            logger.debug(e)
-            client = TelegramClient(f"{session_path}/{e}")
-            logger.info(f"{client}")
-            tdesk = await client.ToTDesktop(flag=UseCurrentSession)
-            logger.info(f"{tdesk}")
-            logger.info(f"Saving Tdata/tdata")
-            tdata_path = read_config_tdata()
-            tdesk.SaveTData(f"{tdata_path}/{e}/tdata")
-
-    elif user_input == '2':
+    elif user_input == '2':  # Путь к папке с сессиями
         print('[chartreuse2]Запись пути к папке с сессиями')
-
         user_input = input("Введите путь к папке с сессиями: ")
         session_path = rf"{user_input}"
         write_config(session_path)
 
-    elif user_input == '3':
+    elif user_input == '3':  # Путь к папке с tdata
         print('[chartreuse2]Запись пути к папке с tdata')
-
         user_input = input("Введите путь к папке с tdata: ")
         tdata_path = rf"{user_input}"
         write_config2(tdata_path)
 
-    elif user_input == '4':
-
+    elif user_input == '4':  # Запуск конвертера в асинхронном режиме
         print('[chartreuse2] Запуск конвертации в асинхронном режиме')
-        # Запускаем задачу на чтение файла
-        # await asyncio.create_task(wait_and_read('session'))
-        await wait_and_read()
+        await wait_and_read()  # Запускаем задачу на чтение файла
 
 
 asyncio.run(main())
