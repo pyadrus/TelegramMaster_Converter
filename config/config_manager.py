2908a7b8124ca35589af7dfb8bad272a13898583
vitaliyzh92 <zh.vitaliy92@yandex.ru>
2024-03-01 21:22:10 +0300

    Оптимизация кода

--- config/config_manager.py
+++ config/config_manager.py
@@ -2,28 +2,47 @@ import json
 
 
 def read_config():
-    """Reads the path from the JSON file."""
+    """
+    Считывает путь из файла JSON.
+    :return: dict
+    """
     with open('session_path.json', 'r', encoding='utf-8') as json_file:
         config = json.load(json_file)
         return config['session_path']
 
 
 def read_config_tdata():
-    """Reads the path from the JSON file."""
+    """
+    Считывает путь из файла JSON.
+    :return: dict
+    """
     with open('tdata_path.json', 'r', encoding='utf-8') as json_file:
         config = json.load(json_file)
         return config['tdata_path']
 
 
 def write_config(session_path):
-    """Writes the path to the JSON file."""
+    """
+    Записывает путь к файлу JSON.
+    :param session_path: str
+    :return: None
+    """
     data = {'session_path': session_path}
     with open('session_path.json', 'w+', encoding='utf-8') as json_file:
         json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 
 def write_config2(tdata_path):
-    """Writes the path to the JSON file."""
+    """
+    Записывает путь к файлу JSON.
+    :param tdata_path: str
+    :return: None
+    """
     data = {'tdata_path': tdata_path}
     with open('tdata_path.json', 'w+', encoding='utf-8') as json_file:
         json.dump(data, json_file, ensure_ascii=False, indent=4)
+
+
+if __name__ == '__main__':
+    read_config()
+    read_config_tdata()
